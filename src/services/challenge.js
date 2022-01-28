edb9e38673283f1e2fe70014b25cd8e5011959e8
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2022-01-28 11:09:33 +0200

    add tags

--- src/services/challenge.js
+++ src/services/challenge.js
@@ -43,6 +43,7 @@ export async function createChallenge() {
     legacy: {
       selfService: true,
     },
+    tags: ["Other"],
     discussions: [
       {
         name: "new-self-service-project",
