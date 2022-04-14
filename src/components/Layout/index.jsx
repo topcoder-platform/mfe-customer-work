54b1e3529efd58907fb997e5075fe468e1f47334
Dimitrina Sirakova <pavlova.dimitrina@gmail.com>
2022-04-14 21:55:53 +0300

    Deleted the comment code.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -8,27 +8,8 @@ import "./styles.module.scss";
  * Block Layout
  */
 const Layout = ({ sidebar, PageComponent, ...routeProps }) => {
-
-  // this will be used when implementing PROD-1559
-
-  // const [size, mainRef] = useTargetSize();
-  // const asideRef = useRef();
-  // const height = size && size.height;
-
-  // useEffect(() => {
-  //   if (height) {
-  //     asideRef.current.style.height = `${height}px`;
-  //   }
-  // }, [height]);
-
   return (
     <div styleName="layout">
-      {/* this will be used when implementing PROD-1559 */}
-      {/* <aside styleName="aside" ref={asideRef}>
-        {sidebar}
-      </aside> */}
-      {/* <main styleName="main" ref={mainRef}> */}
-
       <main>
         <PageComponent {...routeProps} />
       </main>
