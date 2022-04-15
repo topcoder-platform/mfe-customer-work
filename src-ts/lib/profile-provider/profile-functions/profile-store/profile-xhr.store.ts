c29e28ca638eda3e834f256feec372a289a5e300
brooketopcoder <brooke.souza@topcoder.com>
2022-04-15 16:07:04 -0700

    PROD-1606 clean-up #time 5m

--- src-ts/lib/profile-provider/profile-functions/profile-store/profile-xhr.store.ts
+++ src-ts/lib/profile-provider/profile-functions/profile-store/profile-xhr.store.ts
@@ -8,6 +8,8 @@ export function get(handle: string): Promise<UserProfile> {
     return xhrGetAsync<UserProfile>(profileUrl(handle))
 }
 
-export function putName(handle: string, profile: EditNameRequest): Promise<UserProfile> {
-    return xhrPutAsync<EditNameRequest, UserProfile>(profileUrl(handle), profile)
+// TODO: this should prob be a patch, not a put, bc the
+// request body is just a partial profile
+export function putName(handle: string, request: EditNameRequest): Promise<UserProfile> {
+    return xhrPutAsync<EditNameRequest, UserProfile>(profileUrl(handle), request)
 }
