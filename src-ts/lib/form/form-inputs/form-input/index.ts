d5e194bd52e7bfb6c3bd21c5f45ca63622a73010
brooketopcoder <brooke.souza@topcoder.com>
2022-04-15 16:00:11 -0700

    PROD-1606 #comment clean-up #time 5m

--- src-ts/lib/form/form-inputs/form-input/index.ts
+++ src-ts/lib/form/form-inputs/form-input/index.ts
@@ -1,4 +1,4 @@
+export * from './form-input-autcomplete-option.enum'
 export { inputOptional } from './input-wrapper'
 export * from './input-text'
 export * from './input-textarea'
-export * from './form-input-autcomplete-option.enum'
