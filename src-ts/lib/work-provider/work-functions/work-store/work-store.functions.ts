6f928606465fce831d3956863a5221b68b643e64
brooketopcoder <brooke.souza@topcoder.com>
2022-04-21 15:20:20 -0700

    PROD-1628 clean-up #time 5m

--- src-ts/lib/work-provider/work-functions/work-store/work-store.functions.ts
+++ src-ts/lib/work-provider/work-functions/work-store/work-store.functions.ts
@@ -2,12 +2,12 @@ import { xhrDeleteAsync, xhrGetAsync } from '../../../functions'
 import { Page } from '../../../pagination'
 
 import { Challenge } from './challenge.model'
-import { deleteWorkUrl, getWorkUrl } from './work-url.config'
+import { deleteUrl, getUrl } from './work-url.config'
 
 export async function deleteAsync(workId: string): Promise<void> {
-    return xhrDeleteAsync(deleteWorkUrl(workId))
+    return xhrDeleteAsync(deleteUrl(workId))
 }
 
 export async function getAsync(handle: string, page: Page): Promise<Array<Challenge>> {
-    return xhrGetAsync<Array<Challenge>>(getWorkUrl(handle, page))
+    return xhrGetAsync<Array<Challenge>>(getUrl(handle, page))
 }
