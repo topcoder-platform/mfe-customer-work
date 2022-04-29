e34d8860b6e207a2d6d080877f6990f58417f527
brooketopcoder <brooke.souza@topcoder.com>
2022-04-29 09:38:07 -0700

    PROD-1521 clean-up #time 5m

--- src-ts/lib/table/Table.tsx
+++ src-ts/lib/table/Table.tsx
@@ -18,7 +18,7 @@ const Table: <T extends { [propertyName: string]: any }>(props: TableProps<T>) =
 
         const headerRow: Array<JSX.Element> = props.columns
             .map(col => (
-                <th className={classNames(styles.th, styles[col.type])}>
+                <th className={styles.th}>
                     {col.label}
                     <Tooltip tooltip={col.tooltip} />
                 </th>
