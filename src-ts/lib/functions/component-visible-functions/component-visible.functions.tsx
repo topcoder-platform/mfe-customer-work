9ac4a185255ad5052343046d34f9e2b091e05687
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-04-29 11:46:40 +0300

    Refactor Tooltip component, allow for dynamic trigger,  allow trigger on click & mosue hover

--- src-ts/lib/functions/component-visible-functions/component-visible.functions.tsx
+++ src-ts/lib/functions/component-visible-functions/component-visible.functions.tsx
@@ -1,28 +1,53 @@
-import { Dispatch, MutableRefObject, SetStateAction, useEffect, useRef, useState } from 'react'
+import { MouseEvent as RMouseEvent, MouseEventHandler, MutableRefObject, useCallback, useEffect, useRef } from 'react'
 
-export interface ComponentVisible {
-    isComponentVisible: boolean
-    ref: MutableRefObject<any>
-    setIsComponentVisible: Dispatch<SetStateAction<boolean>>
-}
-
-export function useHideClickOutside(isVisible: boolean): ComponentVisible {
-
-    const [isComponentVisible, setIsComponentVisible]: [boolean, Dispatch<SetStateAction<boolean>>]
-        = useState(isVisible)
-
-    const ref: MutableRefObject<any> = useRef(undefined)
-
-    function onClick(event: globalThis.MouseEvent): void {
-        setIsComponentVisible(!!ref.current?.contains(event.target))
-    }
+/**
+ * Registers an outside click event handler, and calls the callback on click outside
+ * @param el Html element to register the event habdler for
+ * @param cb Callback function to be called on click outside of provided element
+ */
+export function useClickOutside(el: HTMLElement|null, cb: (ev: MouseEvent) => void): void {
+    const handleClick: (ev: MouseEvent) => void = useCallback((ev: MouseEvent) => {
+        if (el && !el.contains(ev.target as unknown as Node)) {
+            cb(ev)
+        }
+    }, [cb, el])
 
     useEffect(() => {
-        document.addEventListener('click', onClick, true)
-        return () => {
-            document.removeEventListener('click', onClick, true)
+        if (!el) {
+            document.removeEventListener('click', handleClick)
+            return
         }
-    }, [])
 
-    return { ref, isComponentVisible, setIsComponentVisible }
+        document.addEventListener('click', handleClick)
+        return () => {
+            document.removeEventListener('click', handleClick)
+        }
+    }, [el, handleClick])
+}
+
+export interface UseHoverElementValue {
+    onMouseEnter: MouseEventHandler<HTMLDivElement>
+    onMouseLeave: MouseEventHandler<HTMLDivElement>
+}
+
+/**
+ * Registers an outside click event handler, and calls the callback on click outside
+ * @param el Html element to register the event habdler for
+ * @param cb Callback function to be called on click outside of provided element
+ */
+export function useOnHoverElement(el: HTMLElement|null, cb: (isVisible: boolean) => void): UseHoverElementValue {
+    const counter: MutableRefObject<number> = useRef(0)
+
+    const handleover: (ev: RMouseEvent<Element, MouseEvent>) => void = useCallback((ev: RMouseEvent<Element, MouseEvent>) => {
+        const nextVal: number = counter.current + (ev.type === 'mouseenter' ? 1 : -1)
+        if (!!nextVal !== !!counter.current) {
+            cb(!!nextVal)
+        }
+        counter.current = nextVal
+    }, [cb, el])
+
+    return {
+        onMouseEnter: handleover,
+        onMouseLeave: handleover,
+    }
 }
