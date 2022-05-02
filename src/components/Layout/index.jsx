e69e0a331f27e0c79350ef8d19b11ca522d87a84
brooketopcoder <brooke.souza@topcoder.com>
2022-05-02 15:29:19 -0700

    Revert "PROD-1521 clean up #time 5m"
    This reverts commit e192db62f3f74893099c8da9e8879e1efa3282c9.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PT from "prop-types";
+import { useTargetSize } from "utils/hooks/useTargetSize";
+
+import "./styles.module.scss";
 
 import "./styles.module.scss";
 
