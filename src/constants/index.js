d51ce2c9431a86f16be4751b30a20e0edc94d221
Marios Kranitsas <marioskranitsas1999@gmail.com>
2022-05-17 12:30:47 +0300

    clean up

--- src/constants/index.js
+++ src/constants/index.js
@@ -385,7 +385,6 @@ export const webWorkTypes = [
   },
   {
     title: "Website Design",
-    reviewAndPaymentTitle: "REVIEW & PAYMENT",
     subTitle: "Create a beautiful custom visual design for your website. Specify the scope and device types, your vision, and receive up to 5 modern designs.",
     price: 499,
     // stickerPrice: 499,
