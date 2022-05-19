369f0f58d83f95d0db2499817f583d79dc69fe64
Brooke <brooke.souza@topcoder.com>
2022-05-19 16:46:13 -0700

    PROD-1999 #comment add support for Problem statement work on the dashboard #time 30m

--- src-ts/lib/work-provider/work-functions/work-factory/work-type.enum.ts
+++ src-ts/lib/work-provider/work-functions/work-factory/work-type.enum.ts
@@ -2,5 +2,6 @@ export enum WorkType {
     data = 'Data Exploration',
     design = 'Website Design',
     findData = 'Find Me Data',
+    problem = 'Problem Statement & Data Advisory',
     unknown = '',
 }
