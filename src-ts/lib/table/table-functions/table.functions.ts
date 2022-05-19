c29d1d8ead03a4a5301717d9115791ac09bb97c9
Brooke <brooke.souza@topcoder.com>
2022-05-19 10:43:20 -0700

    PROD-2013 #comment support default sort direction for columns #time 30m

--- src-ts/lib/table/table-functions/table.functions.ts
+++ src-ts/lib/table/table-functions/table.functions.ts
@@ -1,20 +1,36 @@
 import { Sort } from '../../pagination'
 import { TableColumn } from '../table-column.model'
 
+export function getDefaultSort<T>(columns: ReadonlyArray<TableColumn<T>>): Sort | undefined {
+
+    const defaultSortColumn: TableColumn<T> | undefined = columns.find(col => col.isDefaultSort)
+        || columns.find(col => !!col.propertyName)
+
+    const defaultSort: Sort | undefined = !defaultSortColumn?.propertyName
+        ? undefined
+        : {
+            direction: defaultSortColumn.defaultSortDirection || 'asc',
+            fieldName: defaultSortColumn.propertyName,
+        }
+
+    return defaultSort
+}
+
 export function getSorted<T extends { [propertyName: string]: any }>(
     data: ReadonlyArray<T>,
-    sort: Sort,
-    cols: ReadonlyArray<TableColumn<T>>
+    cols: ReadonlyArray<TableColumn<T>>,
+    sort?: Sort,
 ): ReadonlyArray<T> {
 
     // get the sort column
-    const sortColumn: TableColumn<T> | undefined = cols.find(col => col.propertyName === sort.fieldName)
+    const sortColumn: TableColumn<T> | undefined = !!sort
+        ? cols.find(col => col.propertyName === sort.fieldName)
+        : undefined
 
     const sortedData: Array<T> = [...data]
 
-    // this should never happen, but
-    // if we didn't find a column, don't sort
-    if (!sortColumn) {
+    // if we don't have a column to sort, don't sort
+    if (!sort || !sortColumn) {
         return sortedData
     }
 
