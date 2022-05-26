e4b63af42672f708e5c40ba42e5887fd3a207b99
Brooke <brooke.souza@topcoder.com>
2022-05-26 08:22:19 -0700

    PROD-2037 #comment update comment #time 5m

--- src-ts/lib/profile-provider/profile-functions/profile-factory/profile.factory.ts
+++ src-ts/lib/profile-provider/profile-functions/profile-factory/profile.factory.ts
@@ -7,6 +7,7 @@ export function create(profile: UserProfile, token: TokenModel): UserProfile {
     profile.isCustomer = !!token.roles?.some(role => role === UserRole.customer)
     // TODO: all customers are currently marked as members. we'll need to figure
     // out how to distinguish actual members
+    // https://topcoder.atlassian.net/browse/PROD-2069
     profile.isMember = !!token.roles?.some(role => role === UserRole.member)
     return profile
 }
