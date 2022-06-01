be5af5cf3c826a03de274d0185fa963755d53551
Brooke <brooke.souza@topcoder.com>
2022-06-01 11:24:15 -0700

    PROD-2091 #comment please #time 5m

--- src-ts/tools/work/work-detail-header/WorkFeedback/work-feedback-form.config.ts
+++ src-ts/tools/work/work-detail-header/WorkFeedback/work-feedback-form.config.ts
@@ -51,5 +51,5 @@ export const workFeedbackFormDef: FormDefinition = {
         },
     ],
     subtitle: 'To mark this work as done, please provide feedback on your experience.',
-    successMessage: 'Your feedback has been submitted. If your changes do not appear immediately, reload the page.',
+    successMessage: 'Your feedback has been submitted. If your changes do not appear immediately, please reload the page.',
 }
