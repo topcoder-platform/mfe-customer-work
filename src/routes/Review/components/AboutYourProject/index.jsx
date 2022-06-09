7b32865ef7bdea3e04ad60d68869dbeaaa56ac0b
Maria Mattlin <maria.mattlin@topcoder.com>
2022-06-09 12:06:56 -0700

    PROD-2105 #comment Moved important info section to its own component and added logic to determine where to show it based on if it's mobile or not #time 1h

---
+++ src/routes/Review/components/AboutYourProject/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import HelpBanner from "components/HelpBanner";
+import PageUl from '../../../../components/PageElements/PageUl';
+
+const AboutYourProject = () => {
+    return (
+        <HelpBanner
+            styles={["turqoise", "marginTop24"]}
+            title="Important things to know about your project"
+        >
+            <PageUl>
+                <li>
+                    <strong>
+                        Your Dashboard is your go-to hub for managing your work.
+                    </strong>
+                    &nbsp; From here you can view timelines, details, and a more
+                    important information tied to your work submissions.
+                </li>
+                <li>
+                    <strong>
+                        You can expect members of our community to ask you
+                        questions about this work.
+                    </strong>
+                    &nbsp; From your Work Summary page you’ll see if you have
+                    any outstanding Messages indicated by a red icon. Please
+                    answer questions from our members in a timely and thorough
+                    manner. This will help them deliver high quality results for
+                    you on time!
+                </li>
+                <li>
+                    <strong>
+                        Topcoder experts will curate the best solutions for you.
+                    </strong>
+                    &nbsp; This saves you time and energy wading through
+                    submissions that perhaps aren't of value to you. When your
+                    high-quality submissions are ready, you'll be notified to
+                    download your assets, rate your Topcoder experience, and
+                    officially close out this work.
+                </li>
+            </PageUl>
+        </HelpBanner>
+    )
+};
+
+export default AboutYourProject;
