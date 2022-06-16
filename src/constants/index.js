8704bde72f9d72eeec325daa7e79439ab5f270b4
Brooke <brooke.souza@topcoder.com>
2022-06-16 11:53:40 -0700

    PROD-2208 #comment make find me data visible on select work type page. #time 5m

--- src/constants/index.js
+++ src/constants/index.js
@@ -310,7 +310,7 @@ export const webWorkTypes = [
       : findMeDataConfigs.BASE_PRODUCT_PRICE,
     stickerPrice: findMeDataConfigs.BASE_PRODUCT_PRICE,
     duration: `${findMeDataConfigs.DEFAULT_DURATION} Days`,
-    featured: false,
+    featured: true,
     startRoute: "/self-service/work/new/find-me-data/basic-info",
     helperBannerTitle: "WHAT WILL I RECEIVE?",
     helperBannerContent: (
